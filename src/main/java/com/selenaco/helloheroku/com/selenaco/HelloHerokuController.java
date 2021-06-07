3e46816219b4f905a89c731d067f15b6f8507786
Selena Singleton <ssingle7@gmail.com>
2021-06-07 14:37:56 -0400

    First commit

---
+++ src/main/java/com/selenaco/helloheroku/com/selenaco/HelloHerokuController.java
@@ -0,0 +1,19 @@
+package com.selenaco.helloheroku.com.selenaco;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+    @RestController
+    public class HelloHerokuController {
+        @RequestMapping(value = "/hello", method = RequestMethod.GET)
+        public String doHello() {
+            return "Hello, Heroku";
+        }
+        @RequestMapping(value = "/hi/{name}", method = RequestMethod.GET)
+        public String doHi(@PathVariable String name) {
+            return "What's happening, " + name;
+        }
+}
---
+++ src/test/java/com/selenaco/helloheroku/HelloHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selenaco.helloheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
